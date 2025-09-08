0e54b351f7be8d0c91a7555eeaf74296aa19b1e9
athul0126 <athulrameshnr@gmail.com>
2025-09-08 22:31:52 +0530

    Demo ci-cd app

---
+++ index.js
@@ -0,0 +1,19 @@
+// index.js
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Set view engine and views directory
+app.set('view engine', 'ejs'); // or 'handlebars' depending on your choice
+app.set('views', path.join(__dirname, 'views')); // location of your views folder
+
+
+// Route renders index.ejs with dynamic data
+app.get('/', (req, res) => {
+  res.render('index', { name: 'DevOps CI/CD Pipeline Learner' });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
